f41a71ed71514db0d3091c71edb40bb80f8adcdb
hg14699@naver.com <hangyeoldora>
2023-03-28 10:12:04 +0900

    [ADD] add signup page and sources

---
+++ client/src/pages/SignUp/component/PersonalInfo.js
@@ -0,0 +1,11 @@
+import React, { memo } from 'react';
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+const PersonalInfo = memo(() => {
+  return (
+    <div>PersonalInfo</div>
+  )
+})
+
+export default PersonalInfo;
