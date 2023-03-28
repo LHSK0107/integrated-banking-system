223cccbd358db33579c2c2fa3e199826621d3b1a
hg14699@naver.com <hangyeoldora>
2023-03-28 10:11:35 +0900

    [UPDATE] add route to signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./commons/Navbar";
+import SignUp from "./pages/SignUp/Index";
 const App = () => {
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<></>}/>
-        </Routes>
-      </Router>
+      <div className="container">
+        <div className="inner">
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<></>} />
+              <Route path="/signup" element={<SignUp />} />
+            </Routes>
+          </Router>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
