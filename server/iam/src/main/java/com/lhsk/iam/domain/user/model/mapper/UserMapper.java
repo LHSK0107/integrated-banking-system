9d70d9dab38c3f08ad5a8199ef6f6ff347606f4d
dajung8210@gmail.com <dajung8210@gmail.com>
2023-03-31 22:23:36 +0900

    [UPDATE] implement admin find user list - 회원 전체 리스트 출력

--- server/iam/src/main/java/com/lhsk/iam/domain/user/model/mapper/UserMapper.java
+++ server/iam/src/main/java/com/lhsk/iam/domain/user/model/mapper/UserMapper.java
@@ -1,5 +1,8 @@
 package com.lhsk.iam.domain.user.model.mapper;
 
+
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.lhsk.iam.domain.user.model.vo.UserVO;
@@ -7,5 +10,8 @@ import com.lhsk.iam.domain.user.model.vo.UserVO;
 @Repository
 public interface UserMapper {
 	
-	void signup(UserVO userVO);;
+	void signup(UserVO userVO);
+
+	List<UserVO> findAllUser();
+
 }
