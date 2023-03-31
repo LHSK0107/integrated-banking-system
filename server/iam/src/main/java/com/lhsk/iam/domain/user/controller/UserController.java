53bf151fc59517c66d1576277dd6aab3680f3231
dajung8210@gmail.com <dajung8210@gmail.com>
2023-03-31 22:23:36 +0900

    [UPDATE] implement admin find user list - 회원 전체 리스트 출력

--- server/iam/src/main/java/com/lhsk/iam/domain/user/controller/UserController.java
+++ server/iam/src/main/java/com/lhsk/iam/domain/user/controller/UserController.java
@@ -1,10 +1,11 @@
 package com.lhsk.iam.domain.user.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.lhsk.iam.domain.user.model.vo.UserVO;
 import com.lhsk.iam.domain.user.service.UserService;
@@ -13,7 +14,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class UserController {
 	// 생성자 주입
@@ -29,10 +30,16 @@ public class UserController {
 	@PostMapping("/user/signup")
 	public String signup(@RequestBody UserVO userVO) {
 		log.info("UserController.signup");
-		log.info("userVO"+userVO);
+//		log.info("userVO: "+userVO);
 		userService.signup(userVO);
 		return "index";
 	}
-	
+
+	// 회원 리스트 출력
+	@GetMapping("/user/list")
+	public List<UserVO> findAllUser() {
+		log.info("UserController.userList");
+		return userService.findAllUser();
+	}
 	
 }
--- server/iam/src/main/java/com/lhsk/iam/domain/user/service/UserService.java
+++ server/iam/src/main/java/com/lhsk/iam/domain/user/service/UserService.java
@@ -1,12 +1,17 @@
 package com.lhsk.iam.domain.user.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.lhsk.iam.domain.user.model.mapper.UserMapper;
 import com.lhsk.iam.domain.user.model.vo.UserVO;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -17,6 +22,14 @@ public class UserService {
 		// mapper의 signup메서드 호출 (조건 : VO객체를 넘겨줘야 함)
 		userMapper.signup(userVO);
 	}
+
+	public List<UserVO> findAllUser() {
+		// userVO 객체를 userList에 담아서 리턴
+		List<UserVO> userList = userMapper.findAllUser();
+		log.info("userList size: "+userList.size());
+		return userList;
+	}
+
 	
 
 }
