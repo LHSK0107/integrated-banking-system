e791d10f5fe54bd7eddad396714b69df04a912aa
poohharry@naver.com <poohharry@naver.com>
2023-03-31 10:52:35 +0900

    [ADD]: 중간 merge

--- server/iam/src/main/java/com/lhsk/iam/IamApplication.java
+++ server/iam/src/main/java/com/lhsk/iam/IamApplication.java
@@ -1,9 +1,11 @@
 package com.lhsk.iam;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages =  "com.lhsk.iam.domain.user.model.mapper")
 public class IamApplication {
 
 	public static void main(String[] args) {
--- server/iam/src/main/java/com/lhsk/iam/domain/user/model/vo/UserVO.java
+++ server/iam/src/main/java/com/lhsk/iam/domain/user/model/vo/UserVO.java
@@ -1,8 +1,27 @@
 package com.lhsk.iam.domain.user.model.vo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import lombok.Data;
 
 @Data
 public class UserVO {
+	private int user_no;
+	private String user_code; // (ROLE_USER or ROLE_ADMIN)
+	private String id;
+	private String password;
+	private String name;
+	private String dept;
+	private String email;
+	private String phone;
 	
+	public List<String> getUserCodeList() {
+		if(this.user_code.length()>0) {
+			return Arrays.asList(this.user_code);
+		}
+		return new ArrayList<>();
+			
+	}
 }
