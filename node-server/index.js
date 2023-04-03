e2c2868e87442d38d995df8e7e665c88db5716e4
hg14699@naver.com <hangyeoldora>
2023-04-03 15:45:36 +0900

    [UPDATE] modify users api part

--- node-server/index.js
+++ node-server/index.js
@@ -6,27 +6,10 @@ app.use(cors());
 const db = require("./models");
 app.use(express.static("public"));
 require("dotenv").config();
-const axios = require('axios');
 
-const url="https://scloudadmin.appplay.co.kr/gw/ErpGateWay";
-const data={
-    API_ID: "2410",
-    API_KEY: process.env.API_KEY,
-    ORG_NO: "BUSAN",
-    BIZ_NO: process.env.BIZ_NO,
-    REQ_DATA: {
-      ACCT_NO: ""
-    }
-};
-app.use("/inquiry",()=>{
-  axios.post(url, JSON.stringify(data), {
-  headers:{
-      "Content-Type": "applcation/json",
-      Cookie:
-        "JSESSIONID=Epy5I61ifvDSx0rmXMKULOm2QNADpUIW4Wgj2N84.scloudadmin; SCOUTER=x5ogf14l73uv5k",
-  }},
- ).then((res)=>{console.log(res.data.RESP_DATA.REC);return res.data.RESP_DATA.REC});
-});
+const inquiryRouter = require("./routes/Inquiry");
+app.use("/inquiry", inquiryRouter);
+
 const usersRouter = require("./routes/Users");
 app.use("/auth", usersRouter);
 
--- node-server/routes/Users.js
+++ node-server/routes/Users.js
@@ -1,20 +1,19 @@
 const express=require('express');
 const router=express.Router();
+const {Users} = require("../models");
+router.post("/signup", (req,res)=>{
+    const {id,password,name,dept,email,phone}=req.body;
+    console.log({id,password,name,dept,email,phone});
+    Users.create({
+        id: id,
+        password:password,
+        name:name,
+        dept: dept,
+        email: email,
+        phone:phone
+    });
 
-router.post("/signup",async (req,res)=>{
-    // const {id,password,name,dept,email,phone}=req.body;
-    // console.log({id,password,name,dept,email,phone});
-    // Users.create({
-    //     id: id,
-    //     password:password,
-    //     name:name,
-    //     dept: dept,
-    //     email: email,
-    //     phone:phone
-    // });
-    const data = req.body;
-    await Users.create(data);
-    res.json({"status":"success"});
+    res.send("success");
 });
 router.get("/signup",(req,res)=>{
     res.send("<h1>success</h1>");
