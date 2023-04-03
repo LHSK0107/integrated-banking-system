9de8d0e6731a38adcc0a26bd1c1b23508b50ce17
hg14699@naver.com <hangyeoldora>
2023-04-03 14:03:52 +0900

    [UPDATE] configuration about access mysql

--- node-server/routes/Users.js
+++ node-server/routes/Users.js
@@ -1,17 +1,20 @@
 const express=require('express');
 const router=express.Router();
 
-router.post("/signup",(req,res)=>{
-    const {id,password,name,dept,email,phone}=req.body;
-    Users.create({
-        id: id,
-        password:password,
-        name:name,
-        dept: dept,
-        email: email,
-        phone:phone
-    });
-    res.json("success");
+router.post("/signup",async (req,res)=>{
+    // const {id,password,name,dept,email,phone}=req.body;
+    // console.log({id,password,name,dept,email,phone});
+    // Users.create({
+    //     id: id,
+    //     password:password,
+    //     name:name,
+    //     dept: dept,
+    //     email: email,
+    //     phone:phone
+    // });
+    const data = req.body;
+    await Users.create(data);
+    res.json({"status":"success"});
 });
 router.get("/signup",(req,res)=>{
     res.send("<h1>success</h1>");
