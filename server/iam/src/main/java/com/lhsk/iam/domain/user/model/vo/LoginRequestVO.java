1f35d615f2f81bb123cf65ba07cc23a3fb67a145
loollool22 <96961950+loollool22@users.noreply.github.com>
2023-04-03 14:19:08 +0900

    Update LoginRequestVO.java
    fix annotation

--- server/iam/src/main/java/com/lhsk/iam/domain/user/model/vo/LoginRequestVO.java
+++ server/iam/src/main/java/com/lhsk/iam/domain/user/model/vo/LoginRequestVO.java
@@ -1,8 +1,16 @@
 package com.lhsk.iam.domain.user.model.vo;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class LoginRequestVO {
 	private String username;
 	private String password;
