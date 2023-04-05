d16c79cbab72042b5f2ac4c92dc893c163d137a3
dajung8210@gmail.com <dajung8210@gmail.com>
2023-04-05 16:23:31 +0900

    [UPDATE] API URL 및 전반적인 코드  수정
    1. UserVO.java
    	1-1. @ToString 삭제
    	1-2. setter 수정 : @Setter 삭제, userNo를 제외하고 setter로 접근 가능하도록 수정
    2. WithoutUserCodeUserVO.java -> DetailUserVO.java 로 클래스명 변경 / 주석 추가
    3. UpdateUserVO.java
    	3-1. 주석 추가
    	3-2. setter 수정 : @Setter 삭제, userNo를 제외하고 setter로 접근 가능하도록 수정
    4. userMapper.xml
    	4-1. updateUser : if 조건 추가
    	4-2. findAllUser : returnType을 DetailUserVO로 변경
    5. api 주소 변경
    	user -> users
    6. UserService.java
    	상태 값을 controller로 넘겨주는 코드 추가

--- server/iam/src/main/java/com/lhsk/iam/domain/user/service/UserService.java
+++ server/iam/src/main/java/com/lhsk/iam/domain/user/service/UserService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import com.lhsk.iam.domain.user.model.mapper.UserMapper;
 import com.lhsk.iam.domain.user.model.vo.UpdateUserVO;
 import com.lhsk.iam.domain.user.model.vo.UserVO;
-import com.lhsk.iam.domain.user.model.vo.WithoutUserCodeUserVO;
+import com.lhsk.iam.domain.user.model.vo.DetailUserVO;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,39 +16,52 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-
+	// 생성자 주입
 	private final UserMapper userMapper;
 	
+	// id 중복체크
 	public boolean checkDuplicateId(String id) {
 		log.info("Duplicate id count: "+userMapper.checkDuplicateId(id));
 		if(userMapper.checkDuplicateId(id) > 0) return true;
 		else return false;
 	}
 	
-	public void signup(UserVO userVO) {
-		// mapper의 signup메서드 호출 (조건 : VO객체를 넘겨줘야 함)
-		userMapper.signup(userVO);
+	// 회원가입
+	public String signup(UserVO userVO) {
+		try {
+			// mapper의 signup메서드 호출 (조건 : VO객체를 넘겨줘야 함)
+			userMapper.signup(userVO);			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "fail";
+		}
+		return "ok";
 	}
 
-	public void updateUser(UpdateUserVO updateUserVO) {
-		userMapper.updateUser(updateUserVO);
+	// 회원정보 수정
+	public String updateUser(UpdateUserVO updateUserVO) {
+		if (userMapper.updateUser(updateUserVO) > 0) return "ok";
+		else return "fail";
 	}
 
-	public List<UserVO> findAllUser() {
+	// 회원 삭제
+	public String deleteUser(int userNo) {
+		if (userMapper.deleteUser(userNo) > 0) return "ok";
+		else return "fail";
+	}
+
+	// 회원 리스트 (ROLE_ADMIN, ROLE_MANAGER)
+	public List<DetailUserVO> findAllUser() {
 		// userVO 객체를 userList에 담아서 리턴
-		List<UserVO> userList = userMapper.findAllUser();
+		List<DetailUserVO> userList = userMapper.findAllUser();
 		log.info("userList size: "+userList.size());
 		return userList;
 	}
 
-	public WithoutUserCodeUserVO findByUserNo(int userNo) {
+	// 회원 상세조회
+	public DetailUserVO findByUserNo(int userNo) {
 		return userMapper.findByUserNo(userNo);
 	}
-
-	public void deleteUser(int userNo) {
-		userMapper.deleteUser(userNo);
-	}
-
 
 
 	
