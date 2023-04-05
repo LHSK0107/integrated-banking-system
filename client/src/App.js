0905421737cd4450278773399e58c0b33d24046f
hg14699@naver.com <hangyeoldora>
2023-04-05 09:16:17 +0900

    [ADD] add index page files

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import SignUp from "./pages/SignUp/Index";
 import LogIn from "./pages/LogIn/Index";
 import Inquiry from "./pages/Inquiry/Index";
 import NotFound from "./pages/404/Index";
+import Index from "./pages/Index/Index";
 
 const App = () => {
   return (
@@ -13,7 +14,7 @@ const App = () => {
         <Router>
           <Navbar />
           <Routes>
-            <Route path="/" element={<></>} />
+            <Route path="/" element={<><Index/></>} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/login" element={<LogIn />} />
             <Route path="/inquiry" element={<Inquiry/>}/>
