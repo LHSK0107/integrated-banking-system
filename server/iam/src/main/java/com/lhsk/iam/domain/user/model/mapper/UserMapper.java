3557be579d4a030e7b3b6ac760bb4b772b6111dc
dajung8210@gmail.com <dajung8210@gmail.com>
2023-04-05 16:23:31 +0900

    [UPDATE] API URL 및 전반적인 코드  수정
    1. UserVO.java
    	1-1. @ToString 삭제
    	1-2. setter 수정 : @Setter 삭제, userNo를 제외하고 setter로 접근 가능하도록 수정
    2. WithoutUserCodeUserVO.java -> DetailUserVO.java 로 클래스명 변경 / 주석 추가
    3. UpdateUserVO.java
    	3-1. 주석 추가
    	3-2. setter 수정 : @Setter 삭제, userNo를 제외하고 setter로 접근 가능하도록 수정
    4. userMapper.xml
    	4-1. updateUser : if 조건 추가
    	4-2. findAllUser : returnType을 DetailUserVO로 변경
    5. api 주소 변경
    	user -> users
    6. UserService.java
    	상태 값을 controller로 넘겨주는 코드 추가

--- server/iam/src/main/java/com/lhsk/iam/domain/user/model/mapper/UserMapper.java
+++ server/iam/src/main/java/com/lhsk/iam/domain/user/model/mapper/UserMapper.java
@@ -7,21 +7,24 @@ import org.springframework.stereotype.Repository;
 
 import com.lhsk.iam.domain.user.model.vo.UpdateUserVO;
 import com.lhsk.iam.domain.user.model.vo.UserVO;
-import com.lhsk.iam.domain.user.model.vo.WithoutUserCodeUserVO;
+import com.lhsk.iam.domain.user.model.vo.DetailUserVO;
 
 @Repository
 public interface UserMapper {
 	
-	void signup(UserVO userVO);
-
-	void updateUser(UpdateUserVO updateUserVO);
-
-	List<UserVO> findAllUser();
-	
-	WithoutUserCodeUserVO findByUserNo(int userNo);
-
-	void deleteUser(int userNo);
-
+	// id 중복체크
 	int checkDuplicateId(String id);
+	// 회원가입
+	int signup(UserVO userVO);
+	// 회원정보 수정
+	int updateUser(UpdateUserVO updateUserVO);
+	// 회원 삭제
+	int deleteUser(int userNo);
+	// 회원 상세조회
+	List<DetailUserVO> findAllUser();
+	// 회원 삭제
+	DetailUserVO findByUserNo(int userNo);
+
+
 	
 }
--- server/iam/src/main/java/com/lhsk/iam/domain/user/model/vo/UserVO.java
+++ server/iam/src/main/java/com/lhsk/iam/domain/user/model/vo/UserVO.java
@@ -4,21 +4,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 public class UserVO {
-	
+
 	private int userNo;
 	private String userCode;
 	private String id;
@@ -28,7 +23,15 @@ public class UserVO {
 	private String email;
 	private String phone;
 	
-
+	// 회원번호(userNo)는 setter를 통한 접근을 제한한다.
+	public void setUserCode(String userCode) { this.userCode = userCode; }
+	public void setId(String id) { this.id = id; }
+	public void setPassword(String password) { this.password = password; }
+	public void setName(String name) { this.name = name; }
+	public void setDept(String dept) { this.dept = dept; }
+	public void setEmail(String email) { this.email = email; }
+	public void setPhone(String phone) { this.phone = phone; }
+	
 	public List<String> getUserCode() {
 		if(this.userCode.length()>0) {
 			return Arrays.asList(this.userCode);
