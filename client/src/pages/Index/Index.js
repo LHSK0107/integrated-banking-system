4796e802d91db669d1a48e7290db6575c6f1d2f3
hg14699@naver.com <hangyeoldora>
2023-04-05 09:16:17 +0900

    [ADD] add index page files

---
+++ client/src/pages/Index/Index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <div>
+        <section className="main_slider_secion"></section>
+        <section className="main_introduce_secion">
+            introduce
+        </section>
+        <section className="main_image_secion">
+          image
+        </section>
+        <section className="main_notice_secion">
+          notice
+        </section>
+        <section className="main_slider_secion">
+          slider
+        </section>
+    </div>
+  )
+};
+
+export default Index;
