688b8dfb164d3978e7370a63448af333132003bd
Yell <syeri96@gmail.com>
2023-04-06 16:59:01 +0900

    [ADD] footer component

---
+++ client/src/commons/Footer.js
@@ -0,0 +1,14 @@
+import "./Common.css";
+
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../assets/brand/logo.png";
+
+const Footer = () => {
+
+  return (
+    <footer></footer>
+  );
+};
+
+export default Footer;
