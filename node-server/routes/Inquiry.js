7666a39391d908771a4c23efa192fa1ca68f964a
hg14699@naver.com <hangyeoldora>
2023-04-09 03:50:49 +0900

    [UPDATE] Add detailed account inquiry api

--- node-server/routes/Inquiry.js
+++ node-server/routes/Inquiry.js
@@ -14,13 +14,41 @@ const data={
     }
 };
 router.get("/getAccountList",(req, res)=>{
+  axios.post(url,data)
+  .then((response)=>{
+    res.json(response.data);
+  })
+});
+// 계좌 조회
+router.get("/getDetailAccountHistory/:acctNo",(req, res)=>{
+  const acctNo = req.params;
   axios.post(url,{
-    API_ID: "2410",
-    API_KEY: "5e46561fa40f43a6b3ba",
+    API_ID: process.env.ACCOUNT_LIST_CODE,
+    API_KEY: process.env.API_KEY,
     ORG_NO: "BUSAN",
-    BIZ_NO: "1078783749",
+    BIZ_NO: process.env.BIZ_NO,
     REQ_DATA: {
-      ACCT_NO: ""
+      ACCT_NO: acctNo
+    }
+  })
+  .then((response)=>{
+    res.json(response.data);
+  })
+});
+// 계좌 거래내역 상세 조회
+router.get("/getDate/:detailNo/:strDate/:endDate",(req, res)=>{
+  const {detailNo, strDate, endDate} = req.params;
+  console.log(`detailNo:${detailNo}, strDate:${strDate}, endDate:${endDate}`);
+  axios.post(url,{
+    API_ID: process.env.ACCOUNT_TRANSACTION_CODE,
+    API_KEY: process.env.API_KEY,
+    ORG_NO: "BUSAN",
+    BIZ_NO: process.env.BIZ_NO,
+    REQ_DATA: {
+      ACCT_NO: detailNo,
+      INQ_STR_DT : strDate,
+      INQ_END_DT : endDate,
+      PAGE_CNT : "10"
     }
   })
   .then((response)=>{
@@ -28,5 +56,4 @@ router.get("/getAccountList",(req, res)=>{
     res.json(response.data);
   })
 });
-
 module.exports = router;
