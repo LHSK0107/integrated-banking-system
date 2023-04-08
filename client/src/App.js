16def182c241979658742c3d1f79a9c8a8a421eb
hg14699@naver.com <hangyeoldora>
2023-04-09 03:46:32 +0900

    [UPDATE] add detail page route

--- client/src/App.js
+++ client/src/App.js
@@ -6,21 +6,33 @@ import LogIn from "./pages/LogIn/Index";
 import Inquiry from "./pages/Inquiry/Index";
 import NotFound from "./pages/404/Index";
 import Index from "./pages/Index/Index";
+import DetailInquiry from "./pages/DetailInquiry/Index";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const App = () => {
+  const client=new QueryClient({defaultOptions:{
+    queries:{
+      refetchOnWindowFocus: false
+    }
+  }})
   return (
     <div className="App">
       <div className="container">
+      <QueryClientProvider client={client}>
         <Router>
           <Navbar />
           <Routes>
-            <Route path="/" element={<><Index/></>} />
+            <Route path="/" element={<Index/>} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/login" element={<LogIn />} />
-            <Route path="/inquiry" element={<Inquiry/>}/>
+            <Route path="inquiry">
+              <Route index element={<Inquiry/>} />
+                <Route path=":acctNo" element={<DetailInquiry />} />
+            </Route>
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Router>
+        </QueryClientProvider>
       </div>
     </div>
   );
