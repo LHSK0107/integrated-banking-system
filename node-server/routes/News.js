f48f4afec23f9226da24232aa269706323109eb5
Yell <syeri96@gmail.com>
2023-04-20 17:21:54 +0900

    [ADD] add dashboard page and highchart lib

--- node-server/routes/News.js
+++ node-server/routes/News.js
@@ -6,8 +6,8 @@ require("dotenv").config();
 const url =
   "https://openapi.naver.com/v1/search/news.json?query=웹케시&display=10&start=1&sort=date";
 const data = {
-  "X-Naver-Client-Id": NAVER_API_CLIENT_ID,
-  "X-Naver-Client-Secret": NAVER_API_CLIENT_SECRET,
+  "X-Naver-Client-Id": process.env.NAVER_API_CLIENT_ID,
+  "X-Naver-Client-Secret": process.env.NAVER_API_CLIENT_SECRET,
 };
 router.get("/getNews", (req, res) => {
   axios
