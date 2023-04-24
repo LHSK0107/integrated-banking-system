75526ef20b952d09061ed6db7b482d384ab1b009
poohharry@naver.com <poohharry@naver.com>
2023-04-24 16:22:58 +0900

    [UPDATE] : 인증메일 유효기간 3분으로 설정

--- server/iam/src/main/java/com/lhsk/iam/domain/report/service/ReportService.java
+++ server/iam/src/main/java/com/lhsk/iam/domain/report/service/ReportService.java
@@ -49,6 +49,21 @@ public class ReportService {
 		 */
 		XSSFWorkbook  workbook = new XSSFWorkbook();
 		Sheet sheet = workbook.createSheet("일일시재보고서");	// 시트이름
+		// 픽셀 단위로 너비 설정
+        int widthInPixels = 15;
+        // 기본 문자 너비
+        int defaultCharWidth = 256;
+        // 픽셀을 문자 너비로 변환 (대략적인 값)
+        int widthInChars = widthInPixels * defaultCharWidth / 7;
+        sheet.setColumnWidth(0, widthInChars);
+        sheet.setColumnWidth(19, widthInChars);
+        // 픽셀 단위로 높이 설정
+        int heightInPixels = 29;
+        // 픽셀을 포인트로 변환 (대략적인 값)
+        float pointsPerPixel = 72f / 96f; // 1픽셀은 대략 0.75포인트입니다.
+        float heightInPoints = heightInPixels * pointsPerPixel;
+		
+		
 		// 셀 스타일 생성
 //		CellStyle cellStyle = workbook.createCellStyle();
 		// 폰트 생성
@@ -56,24 +71,26 @@ public class ReportService {
         bold.setBold(true);	// 볼드처리
         
 		// 제목부분
-		// 셀 병합을 위한 셀 범위 생성 (시작 행, 끝 행, 시작 열, 끝 열)
-		CellRangeAddress cellRangeAddress = new CellRangeAddress(0, 3, 1, 16);
-		// 셀 병합 적용
+		CellRangeAddress cellRangeAddress = new CellRangeAddress(0, 3, 1, 18);
+		
         sheet.addMergedRegion(cellRangeAddress);
-        // 병합된 셀에 값을 설정
-        Row row = sheet.createRow(0);
-        Cell cell = row.createCell(1);
+        
+        Font subjectFont = workbook.createFont();
+        subjectFont.setFontHeightInPoints((short) 18);
+        subjectFont.setBold(true);
+        
         CellStyle subjectStyle = workbook.createCellStyle();
         subjectStyle.setAlignment(HorizontalAlignment.CENTER);
         subjectStyle.setVerticalAlignment(VerticalAlignment.CENTER);
-        bold.setFontHeightInPoints((short) 18);
-        subjectStyle.setFont(bold);
+        subjectStyle.setFont(subjectFont);
+        
+        Row row = sheet.createRow(0);
+        Cell cell = row.createCell(1);
         cell.setCellValue("일일시재보고서");
         cell.setCellStyle(subjectStyle);
-        bold.setFontHeightInPoints((short) 11);
+        
         
         // 기준일부분
-        
         byte[] rgb = new byte[]{(byte) 244, (byte) 244, (byte) 244};
         XSSFColor color244 = new XSSFColor(rgb, null);
         XSSFCellStyle color244Cell = workbook.createCellStyle();
@@ -82,6 +99,7 @@ public class ReportService {
         
         
         row = sheet.createRow(4);
+        row.setHeightInPoints(heightInPoints);
         cellRangeAddress = new CellRangeAddress(4,4,2,18);
         cell = row.createCell(0);
         cell.setCellStyle(color244Cell);
@@ -89,11 +107,16 @@ public class ReportService {
         color244Cell.setFont(bold);
         cell.setCellStyle(color244Cell);
         cell.setCellValue("기준일 : ");
+
+        XSSFCellStyle dateCellStyle = workbook.createCellStyle();
+        dateCellStyle.setFillForegroundColor(color244);
+        dateCellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
         cell = row.createCell(2);
-        color244Cell.setFont(null);
-        cell.setCellStyle(color244Cell);
+        cell.setCellStyle(dateCellStyle);
         cell.setCellValue("2023-04-24");
         sheet.addMergedRegion(cellRangeAddress);
+        cell = row.createCell(19);
+        cell.setCellStyle(dateCellStyle);
         
         
         
