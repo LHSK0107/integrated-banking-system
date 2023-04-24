3b382ccb618773aa9992cf52ded6dc17c1f09703
dajung8210@gmail.com <dajung8210@gmail.com>
2023-04-24 15:25:56 +0900

    [UPDATE] findByAcctNo 수정 중

--- server/iam/src/main/java/com/lhsk/iam/domain/account/service/AccountService.java
+++ server/iam/src/main/java/com/lhsk/iam/domain/account/service/AccountService.java
@@ -10,6 +10,7 @@ import com.lhsk.iam.domain.account.model.mapper.AccountMapper;
 import com.lhsk.iam.domain.account.model.vo.AccountVO;
 import com.lhsk.iam.domain.account.model.vo.InoutRequestVO;
 import com.lhsk.iam.domain.account.model.vo.InoutVO;
+import com.lhsk.iam.domain.user.service.UserService;
 import com.lhsk.iam.global.encrypt.AesGcmEncrypt;
 
 import lombok.RequiredArgsConstructor;
@@ -26,6 +27,10 @@ public class AccountService {
 	
 	@Value("${aes.secret}")
 	private String key;
+	@Value("${aes.iv}")
+	private String ivString;
+	
+	private byte[] iv = new byte[12];
 	
 // 이미 외부API를 조회하고 DB에 저장했기 때문에 DB에서만 조회하여 찾으면 된다. -> ?
 	// 계좌 리스트 (ROLE_ADMIN, ROLE_MANAGER)
@@ -35,7 +40,7 @@ public class AccountService {
 		// accountVO에서 acctNo를 복호화해서 넣음
 		for (AccountVO account : accountList) {
 			try {
-				account.setAcctNo(decryptAcctNo(account));
+				account.setAcctNo(aesGcmEncrypt.decrypt(account.getAcctNo(), key));
 			} catch (GeneralSecurityException e) {
 				throw new RuntimeException("Failed to decrypt acctNo", e); 
 			}
@@ -45,7 +50,16 @@ public class AccountService {
 	}
 
 	public AccountVO findByAcctNo(String acctNo) {
-		return accountMapper.findByAcctNo(acctNo);
+		try {
+			// acctNo를 암호화 하여 동일한 값을 가진 계좌정보 조회 
+			ivToByteArray(acctNo);
+			AccountVO account = accountMapper.findByAcctNo(aesGcmEncrypt.encrypt(acctNo, key, iv));
+			// DB에서 가져온 정보 중, 계좌번호를 평문으로 복호화
+			account.setAcctNo(aesGcmEncrypt.decrypt(account.getAcctNo(), key));
+			return account;
+		} catch (GeneralSecurityException e) {
+			throw new RuntimeException("Failed to encrypt acctNo", e); 
+		}
 	}
 	
 	// 계좌 상세정보
@@ -61,8 +75,14 @@ public class AccountService {
 	
 //	---------------------------------------------------------------------------------------
 	
-	// 계좌번호 복호화
-	public String decryptAcctNo(AccountVO account) throws GeneralSecurityException {
-		return aesGcmEncrypt.decrypt(account.getAcctNo(), key); 
+	// iv property를 byte[]로 변환
+	public byte[] ivToByteArray(String ivString) {
+		// property에서 String으로 받아온 ivString을  ", "을 기준으로 split -> String[]에 저장
+		String[] ivStringArray = ivString.split(", ");
+		// String[] -> byte[]로 번환
+		for (int i = 0; i < iv.length; i++) {
+		    iv[i] = Byte.parseByte(ivStringArray[i]);
+		}
+		return iv;
 	}
 }
