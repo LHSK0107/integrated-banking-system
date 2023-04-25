c0de0d0442643a22275d52c39827583fa6e9b739
Yell <syeri96@gmail.com>
2023-04-25 15:06:15 +0900

    [UPDATE] login and show user info in top nav without expiration time

--- client/src/commons/LogInContext.js
+++ client/src/commons/LogInContext.js
@@ -1,6 +1,7 @@
 import { createContext, useContext } from "react";
 
 export const LogInContext = createContext({
+  token: null,
   loggedUser: {
     id: "",
     name: "",
--- client/src/commons/Navbar.js
+++ client/src/commons/Navbar.js
@@ -1,16 +1,94 @@
 import "./Common.css";
-import React, { useState, useEffect, useCallback, useRef, useContext } from "react";
+import React, {
+  useState,
+  useEffect,
+  useCallback,
+  useRef,
+  useContext,
+} from "react";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/brand/logo.png";
 import { LogInContext } from "./LogInContext";
+import decodeJwt from "../hooks/decodeJwt";
+import axios from "axios";
 
 const Navbar = () => {
-  // context 처리
-  const { loggedUser, setLoggedUser, loggedIn, setLoggedIn } = useContext(LogInContext);
-  console.log(loggedIn);
-  console.log(loggedUser.userCode[0]);
+  const { token, setToken, loggedUser, setLoggedUser, loggedIn, setLoggedIn } =
+    useContext(LogInContext);
+  const navigate = useNavigate();
 
-  const navigate=useNavigate();
+  useEffect(() => {
+    const savedToken = localStorage.getItem("jwt");
+    setToken(savedToken);
+  }, []);
+
+  useEffect(() => {
+    if (token === null) {
+      setLoggedUser({
+        id: "",
+        name: "",
+        exp: "",
+        userCode: "",
+        userNo: "",
+      });
+      setLoggedIn(false);
+    } else {
+      const decodedPayload = decodeJwt(token);
+      setLoggedUser({
+        id: decodedPayload.id,
+        name: decodedPayload.name,
+        exp: decodedPayload.exp,
+        userCode: decodedPayload.userCode,
+        userNo: decodedPayload.userNo,
+      });
+      setLoggedIn(true);
+    }
+  }, [token, setLoggedUser, setLoggedIn]);
+  console.log(loggedUser);
+
+  // 로그아웃
+  const handleLogout = () => {
+    if (window.confirm("로그아웃 하시겠습니까?")) {
+      console.log("로그아웃 완료");
+
+      // axios.post("http://localhost:8080/logout", {}).then((res) => console.log(res)).catch((err) => console.log(err));
+
+      // axios
+      //   .post("http://localhost:8080/logout", {})
+      //   .then((response) => {
+      //     if (response.status === 200) {
+      //       localStorage.removeItem("jwt");
+      //       setLoggedUser({
+      //         id: "",
+      //         name: "",
+      //         exp: "",
+      //         userCode: "",
+      //         userNo: "",
+      //       });
+      //       setLoggedIn(false);
+      //     }
+      //   })
+      //   .catch((error) => {
+      //     if (error) console.log(error);
+      //   })
+      //   .finally(() => {});
+      
+      localStorage.removeItem("jwt");
+      setLoggedUser({
+        id: "",
+        name: "",
+        exp: "",
+        userCode: "",
+        userNo: "",
+      });
+      setLoggedIn(false);
+      navigate("/login");
+    } else {
+      console.log("로그아웃 취소");
+      return false;
+    }
+  };
+
   const [scrollData, setScrollData] = useState(0);
   const navInfoRef = useRef();
   const navMenuRef = useRef();
@@ -62,7 +140,11 @@ const Navbar = () => {
             </div>
             {/* 로그인 체크 부분 */}
             <div className="user_info_wrap">
-              {loggedIn ? <LogoutSection value={loggedUser} /> : <LoginSection />}
+              {loggedIn ? (
+                <LogoutSection value={loggedUser} func={handleLogout} />
+              ) : (
+                <LoginSection />
+              )}
             </div>
           </div>
         </div>
@@ -76,16 +158,16 @@ const Navbar = () => {
             </Link>
             <ul className="menu_list flex">
               <li>
-                <span onClick={()=>navigate("/")}>소개</span>
+                <span onClick={() => navigate("/")}>소개</span>
               </li>
               <li>
-                <span onClick={()=>navigate("/inquiry")}>조회</span>
+                <span onClick={() => navigate("/inquiry")}>조회</span>
               </li>
               <li>
-                <span onClick={()=>navigate("/inquiry")}>보고서</span>
+                <span onClick={() => navigate("/inquiry")}>보고서</span>
               </li>
               <li>
-                <span onClick={()=>navigate("/dashboard")}>대시보드</span>
+                <span onClick={() => navigate("/dashboard")}>대시보드</span>
               </li>
             </ul>
           </div>
@@ -108,23 +190,30 @@ const LoginSection = () => {
   );
 };
 
-const LogoutSection = ({value}) => {
-  const { loggedUser } = value;
+const LogoutSection = (props) => {
   return (
     <div className="login flex align_center">
-      <p>
-        <Link to="/">로그아웃</Link>
+      <p className="login_username">
+        안녕하세요 <span>{props.value.name}</span>님
       </p>
+      <p className="login_exp">{props.value.exp}</p>
+      {/* <p>
+        <Link to="/logout">로그아웃</Link>
+      </p> */}
+      <button className="logout_btn" onClick={props.func}>
+        로그아웃
+      </button>
       <p>
         <Link to="/">개인정보수정</Link>
       </p>
-    {loggedUser.userCode[0]==="ROLE_ADMIN" || loggedUser.userCode[0]==="ROLE_MANAGER" ? 
-      <p>
-        <Link to="/">관리자 페이지</Link>
-      </p>
-      :
-      <></>
-    }
+      {props.value.userCode[0] === "ROLE_ADMIN" ||
+      props.value.userCode[0] === "ROLE_MANAGER" ? (
+        <p>
+          <Link to="/">관리자 페이지</Link>
+        </p>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
