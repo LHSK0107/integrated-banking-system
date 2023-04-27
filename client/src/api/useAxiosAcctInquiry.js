a2e38cce271ec7f0a71d87b2d304396595a5b4bb
hg14699@naver.com <hangyeoldora>
2023-04-27 23:50:46 +0900

    [UPDATE] modify api to set token in header

--- client/src/api/useAxiosAcctInquiry.js
+++ client/src/api/useAxiosAcctInquiry.js
@@ -6,18 +6,28 @@ function useAxiosAcctInquiry ( url ) {
   const [isLoading, setIsLoading]=useState(false);
   const [error, setError]=useState(null);
 
+  const imsiValue = localStorage.getItem("jwt");
+
   useEffect(()=>{
     const fetchController = new AbortController();
     const signal = fetchController.signal;
     setIsLoading(true);
-    axios.get(url,{signal: signal})
-    .then((res)=>{
-      setApiData(res.data.RESP_DATA.REC);
-    })
-    .catch((err)=>setError(`에러 발생 ${err}`))
-    .finally(()=>{
-      setIsLoading(false);
-    })
+    console.log(localStorage.getItem("jwt"));
+    axios
+      .get(url, {
+        signal: signal,
+        headers: {
+          "Authorization":
+            `Bearer ${localStorage.getItem("jwt")}`,
+        },
+      })
+      .then((res) => {
+        setApiData(res.data);
+      })
+      .catch((err) => setError(`에러 발생 ${err}`))
+      .finally(() => {
+        setIsLoading(false);
+      });
     return () => {
       fetchController.abort();
     }
