0c6b74530bd3ac924432e6f49063837cb4f6b815
Yell <syeri96@gmail.com>
2023-04-27 09:44:30 +0900

    [UPDATE] mypage and some design in index and app

--- client/src/pages/Mypage/component/ConfirmPw.js
+++ client/src/pages/Mypage/component/ConfirmPw.js
@@ -7,21 +7,19 @@ import decodeJwt from "../../../hooks/decodeJwt";
 import { LogInContext } from "../../../commons/LogInContext";
 import { useNavigate } from "react-router";
 import { yupResolver } from "@hookform/resolvers/yup";
-import "../mypage.css"
+import "../mypage.css";
 
 export default function ConfirmPw() {
   // 토큰 확인
-  const { token, setToken, loggedUser, setLoggedUser, loggedIn, setLoggedIn } = useContext(LogInContext);
+  const { token, setToken, loggedUser, setLoggedUser, loggedIn, setLoggedIn } =
+    useContext(LogInContext);
   const navigate = useNavigate();
-
-  // 로컬스토리지에서 jwt 가져오기
-  useEffect(() => {
-    const savedToken = localStorage.getItem("jwt");
-    setToken(savedToken);
-  }, []);
 
   // 토큰으로 로그인 context api 세팅
   useEffect(() => {
+    // 로컬스토리지에서 jwt 가져오기
+    const savedToken = localStorage.getItem("jwt");
+    setToken(savedToken);
     if (token === null) {
       // 토큰이 없다면
       setLoggedIn(false);
@@ -41,36 +39,48 @@ export default function ConfirmPw() {
   console.log(loggedUser.userNo);
 
   // 페이지 확인 및 설정
-  const { stepNum, setStepNum, userInfo, setUserInfo } = useContext(StepContext);
+  const { stepNum, setStepNum, userInfo, setUserInfo } =
+    useContext(StepContext);
   console.log(stepNum, userInfo);
 
   // form 유효성 검사
-  const schema = yup.object({
-    password: yup
-      .string()
-      .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[?!@#$%^&*+=-])(?=.*[0-9]).{6,20}$/,"대/소/특수/숫자 포함하여 입력해주세요.")
-      .min(6, "최소 6자 이상 입력해주세요.")
-      .max(20, "최대 20자리까지 입력해주세요.")
-      .required("패스워드를 입력해주세요.")
-  }).required();
-  const {register, handleSubmit, formState:{errors}} = useForm({resolver: yupResolver(schema), mode: "onChange"});
+  const schema = yup
+    .object({
+      password: yup
+        .string()
+        .matches(
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*[?!@#$%^&*+=-])(?=.*[0-9]).{6,20}$/,
+          "대/소/특수/숫자 포함하여 입력해주세요."
+        )
+        .min(6, "최소 6자 이상 입력해주세요.")
+        .max(20, "최대 20자리까지 입력해주세요.")
+        .required("패스워드를 입력해주세요."),
+    })
+    .required();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema), mode: "onChange" });
 
   // axios
   const onSubmit = (data) => {
     console.log(data);
-    axios.post("http://localhost:8080/api/users/checkPass", {
+    axios
+      .post("http://localhost:8080/api/users/checkPass", {
         userNo: loggedUser.userNo,
-        password: data.password
-    }).then((res) => {
-      console.log(res);
-      console.log(res.data);
-        if(res.status === 200 && res.data === true) {
-            setStepNum(stepNum+1);
-        } else if(res.data === false){
-            alert("비밀번호가 맞지 않습니다.")
-            return false;
+        password: data.password,
+      })
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+        if (res.status === 200 && res.data === true) {
+          setStepNum(stepNum + 1);
+        } else if (res.data === false) {
+          alert("비밀번호가 맞지 않습니다.");
+          return false;
         }
-    });
+      });
   };
 
   return (
@@ -79,7 +89,7 @@ export default function ConfirmPw() {
         개인정보를 변경하고 싶으신 경우, 본인확인을 위해 비밀번호를 한 번 더
         입력해주세요.
       </p>
-      <form onSubmit={handleSubmit(onSubmit)}  autoComplete="off">
+      <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
         <input type="password" {...register("password")}></input>
         <button type="submit">확인</button>
       </form>
