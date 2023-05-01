9f4753a1e13f2a8751e46136168b396e7cd32af1
dajung8210@gmail.com <dajung8210@gmail.com>
2023-05-01 10:21:12 +0900

    [UPDATE, FIX] Refactoring user APIs and Add authorization of userCode

--- server/iam/src/main/java/com/lhsk/iam/domain/user/controller/UserController.java
+++ server/iam/src/main/java/com/lhsk/iam/domain/user/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.lhsk.iam.domain.user.controller;
 
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.Cookie;
@@ -37,11 +36,11 @@ public class UserController {
 	private final JwtTokenProvider jwtTokenProvider;
 	private final JwtConfig jwtConfig;
 	
-	// id 중복 체크
+	// id 존재 여부 확인
 	@PostMapping("/api/signup/id")
-	public ResponseEntity<?> checkDuplicateId(@RequestBody UserVO userVO) {
-		log.info("UserController.CheckDuplicateId");
-		boolean flag = userService.checkDuplicateId(userVO.getId());
+	public ResponseEntity<?> checkExistsId(@RequestBody UserVO userVO) {
+		log.info("UserController.CheckExistsId");
+		boolean flag = userService.checkExistsId(userVO.getId());
 		// 상태코드 200과 함께 중복되면 true, 중복되지 않으면 false 반환
 		return new ResponseEntity<>(flag, HttpStatus.OK);
 	}
@@ -57,19 +56,14 @@ public class UserController {
 	
 	// password 일치 확인
 	@PostMapping("/api/users/checkPass")
-	public ResponseEntity<?> checkPassword(
-											@RequestBody Map<String, Object> data, 
-											HttpServletRequest httpServletRequest	
-	) {
+	public ResponseEntity<?> checkPassword(@RequestBody Map<String, Object> data, HttpServletRequest request) {
 		log.info("UserController.CheckPassword");
-		// 토큰으로 userCode parsing
-		String accessToken = httpServletRequest
-								.getHeader("Authorization")
-								.replace(jwtConfig.getTokenPrefix(), "");
+		// Token에서 userCode parsing
+		String accessToken = request.getHeader("Authorization")
+									.replace(jwtConfig.getTokenPrefix(), "");
 		String userCode = jwtTokenProvider.getUserCodeFromToken(accessToken);
 		// userCode가 user, manager, admin일 때 password 일치 확인 가능
-		if (
-			userCode.equals("ROLE_USER" ) || 
+		if (userCode.equals("ROLE_USER" ) || 
 			userCode.equals("ROLE_MANAGER") || 
 			userCode.equals("ROLE_ADMIN")
 		) {
@@ -85,38 +79,175 @@ public class UserController {
 	@PostMapping("/api/signup")
 	public ResponseEntity<?> signup(@RequestBody UserVO userVO) {
 		log.info("UserController.signup");
+		// 회원가입 성공 시, 상태코드 200과 함께 success 반환
 		if (userService.signup(userVO).equals("success"))
 			return ResponseEntity.ok().body("success");
+		// 회원가입 실패 시, 상태코드 400과 함께 success 반환
 		else return ResponseEntity.badRequest().body("fail");
 	}
 	
 	// 회원정보 수정
 	@PutMapping("/api/users")
-	public String updateUser(@RequestBody UpdateUserVO updateUserVO) {
-		log.info("UserController.update"); 
-//		log.info(updateUserVO.toString());
-		return "{\"status\":\""+userService.updateUser(updateUserVO)+"\"}";
+	public ResponseEntity<?> updateUser(@RequestBody UpdateUserVO updateUserVO, HttpServletRequest request) {
+		log.info("UserController.update");
+		
+		// Token에서 userCode와 id parsing
+		String acceessToken = request.getHeader("Authorization")
+									 .replace(jwtConfig.getTokenPrefix(), "");
+		String userCode = jwtTokenProvider.getUserCodeFromToken(acceessToken);
+		String id = jwtTokenProvider.getUsernameFromToken(acceessToken);
+		
+		// userCode가 user, manager, admin일 때 회원정보 수정 가능
+		// 1. user일 때
+		if (userCode.equals("ROLE_USER")) {
+			// 수정하려는 회원과 발급된 토큰의 소유자와 같은 지 확인
+			if (id.equals(userService.findByUserNo(updateUserVO.getUserNo()).getId())) {
+				String flag = userService.updateUser(userCode, updateUserVO);
+				// 쿼리 성공 여부에 따라 다른 상태코드 반환
+				if (flag.equals("success")) { 
+					return new ResponseEntity<>(flag, HttpStatus.OK); }
+				else { 
+					return new ResponseEntity<>(flag, HttpStatus.BAD_REQUEST); }
+			// 수정하려는 회원과 발급된 토큰의 소유자와 다르면 접근 제한
+			} else { 
+				return new ResponseEntity<>("fail", HttpStatus.FORBIDDEN); 
+			}
+		// 2. manager나 admin일 때
+		} else if (userCode.equals("ROLE_MANAGER") || userCode.equals("ROLE_ADMIN")) {
+			String flag = userService.updateUser(userCode, updateUserVO);
+			// 쿼리 성공 여부에 따라 다른 상태코드 반환
+			if (flag.equals("success")) { 
+				return new ResponseEntity<>(flag, HttpStatus.OK); }
+			else { 
+				return new ResponseEntity<>(flag, HttpStatus.BAD_REQUEST); }
+		// 3. userCode가 user, manager, admin이 아닌 경우 접근 제한
+		} else {
+			return new ResponseEntity<>("fail", HttpStatus.FORBIDDEN);
+		}
 	}
 	
 	// 회원 삭제
 	@DeleteMapping("/api/users/{userNo}")
-	public String deleteUser(@PathVariable int userNo) {
-		log.info("UserController.deleteUser"); 
-		return "{\"status\":\""+userService.deleteUser(userNo)+"\"}";
-	}
+	public ResponseEntity<?> deleteUser(@PathVariable int userNo, HttpServletRequest request) {
+		log.info("UserController.deleteUser");
+		// Token에서 userCode와 id parsing
+		String accessToken = request.getHeader("Authorization")
+									 .replace(jwtConfig.getTokenPrefix(), "");
+		String userCode = jwtTokenProvider.getUserCodeFromToken(accessToken);
+		String id = jwtTokenProvider.getUsernameFromToken(accessToken);
+		
+		// userCode가 user, manager, admin일 때 탈퇴 가능
+		// 1. user일 때
+		if (userCode.equals("ROLE_USER" )) {
+			// 삭제하려는 회원과 토큰을 가진 회원이 동일한지 검증
+			if(id.equals((userService.findByUserNo(userNo).getId()))) {
+				String flag = userService.deleteUser(userNo);
+				// 쿼리 성공 여부에 따라 다른 상태 코드 반환
+				if (flag.equals("success")) {
+					return new ResponseEntity<>(flag, HttpStatus.OK); }
+				else {
+					return new ResponseEntity<>(flag, HttpStatus.BAD_REQUEST); }
+			// 동일하지 않으면 접근 제한
+			} else {
+				return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+			}
+		// 2. manager일 때
+		} else if (userCode.equals("ROLE_MANAGER")) {
+			// 2-1. 삭제하려는 user가 자신일 때
+			if(id.equals((userService.findByUserNo(userNo).getId()))) {
+				String flag = userService.deleteUser(userNo);
+				// 쿼리 성공 여부에 따라 다른 상태 코드 반환
+				if (flag.equals("success")) {
+					return new ResponseEntity<>(flag, HttpStatus.OK); }
+				else {
+					return new ResponseEntity<>(flag, HttpStatus.BAD_REQUEST); }
+			// 2-2. 삭제하려는 user가 manager일 때, 접근 제한
+			} else if(userService.findByUserNo(userNo).getUserCode().equals("ROLE_MANAGER")) {
+				return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+			// 2-3. 삭제하려는 user가 admin일 때, 접근 제한
+			} else if(userService.findByUserNo(userNo).getUserCode().equals("ROLE_ADMIN")) {
+				return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+			} else {
+				String flag = userService.deleteUser(userNo);
+				// 쿼리 성공 여부에 따라 다른 상태 코드 반환
+				if (flag.equals("success")) {
+					return new ResponseEntity<>(flag, HttpStatus.OK); }
+				else {
+					return new ResponseEntity<>(flag, HttpStatus.BAD_REQUEST); }
+			}
+		// 3. admin일 때
+		} else if (userCode.equals("ROLE_ADMIN")) {
+			String flag = userService.deleteUser(userNo);
+			// 쿼리 성공 여부에 따라 다른 상태 코드 반환
+			if (flag.equals("success")) {
+				return new ResponseEntity<>(flag, HttpStatus.OK); }
+			else {
+				return new ResponseEntity<>(flag, HttpStatus.BAD_REQUEST); }
+		// 4. userCode가 user, manger, admin이 아닐 때, 접근 제한
+		} else {
+			return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+		}
+	} 
 
 	// 회원 리스트 (ROLE_ADMIN, ROLE_MANAGER)
-	@GetMapping("/api/users")
-	public List<DetailUserVO> findAllUser() {
+	@GetMapping("/api/admin/users")
+	public ResponseEntity<?> findAllUser(HttpServletRequest request) {
 		log.info("UserController.userList");
-		return userService.findAllUser();
+
+		// Token에서 userCode parsing
+		String accessToken = request.getHeader("Authorization");
+		String userCode = jwtTokenProvider.getUserCodeFromToken(accessToken);
+	
+		// 1. manager나 admin일 때
+		if (userCode.equals("ROLE_MANAGER") || userCode.equals("ROLE_ADMIN")) {
+			return new ResponseEntity<>(userService.findAllUser(), HttpStatus.OK);
+		// 2. 그 외는 접근 제한
+		} else {
+			return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+		}
 	}
 	  
-	// 회원 상세조회 (ROLE_USER)
-	@GetMapping("/api/users/{userNo}")
-	public DetailUserVO findByUserNo(@PathVariable int userNo) {	// @PathVariable : 경로 상의 값을 가져올 때 사용 
+	// 회원 상세조회
+	@GetMapping("/api/users/{userNo}")	// @PathVariable : 경로 상의 값을 가져올 때 사용
+	public ResponseEntity<?> findByUserNo(@PathVariable int userNo, HttpServletRequest request) { 
 		log.info("UserController.findByUserNo"); 
-		return userService.findByUserNo(userNo);
+
+		// Token에서 userCode parsing
+		String accessToken = request.getHeader("Authorization");
+		String userCode = jwtTokenProvider.getUserCodeFromToken(accessToken);		
+		String id = jwtTokenProvider.getUsernameFromToken(accessToken);
+		
+		DetailUserVO userInfo = userService.findByUserNo(userNo);
+		
+		// 1. user일 때
+		if (userCode.equals("ROLE_USER" )) {
+			// 조회하려는 회원과 토큰을 가진 회원이 동일한지 검증
+			if(id.equals(userInfo.getId())) {
+				return new ResponseEntity<>(userInfo, HttpStatus.OK); }
+			// 동일하지 않으면 접근 제한
+			else {
+				return new ResponseEntity<>(HttpStatus.FORBIDDEN); }
+		// 2. manager일 때
+		} else if (userCode.equals("ROLE_MANAGER")) {
+			// 2-1. 조회하려는 user가 자신일 때
+			if(id.equals(userInfo.getId())) {
+				return new ResponseEntity<>(userInfo, HttpStatus.OK);
+			// 2-2. 조회하려는 user가 manager일 때, 접근 제한
+			} else if(userInfo.getUserCode().equals("ROLE_MANAGER")) {
+				return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+			// 2-3. 삭제하려는 user가 admin일 때, 접근 제한
+			} else if(userInfo.getUserCode().equals("ROLE_ADMIN")) {
+				return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+			} else {
+				return new ResponseEntity<>(userInfo, HttpStatus.OK); 
+			}
+		// 3. admin일 때
+		} else if (userCode.equals("ROLE_ADMIN")) {
+			return new ResponseEntity<>(userInfo, HttpStatus.OK);
+		// 4. userCode가 user, manger, admin이 아닐 때, 접근 제한
+		} else {
+			return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+		}
 	}
 	
 	// 로그아웃
@@ -142,7 +273,6 @@ public class UserController {
         // 성공적으로 로그아웃 되었다는 응답을 반환합니다.
         return ResponseEntity.ok().body("로그아웃 되었습니다.");
     }
-	
 	
 		
 }
