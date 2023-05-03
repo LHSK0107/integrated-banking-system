7033ef0ccae73a513de3b461e7c40ac4314ca7f2
poohharry@naver.com <poohharry@naver.com>
2023-05-03 22:01:56 +0900

    [ADD] : 메뉴클릭기록 집계 및 조회기능 추가

--- server/iam/src/main/java/com/lhsk/iam/domain/admin/controller/AdminController.java
+++ server/iam/src/main/java/com/lhsk/iam/domain/admin/controller/AdminController.java
@@ -1,14 +1,18 @@
 package com.lhsk.iam.domain.admin.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lhsk.iam.domain.admin.model.vo.LoginHistoryReqeustVO;
+import com.lhsk.iam.domain.admin.model.vo.MenuClickRequestVO;
+import com.lhsk.iam.domain.admin.model.vo.MenuClickVO;
 import com.lhsk.iam.domain.admin.service.AdminService;
 import com.lhsk.iam.domain.user.model.vo.LoginHistoryVO;
 import com.lhsk.iam.global.config.jwt.JwtTokenProvider;
@@ -23,25 +27,31 @@ public class AdminController {
 	
 	// 모든 유저의 로그인 기록 조회
 	@GetMapping("/api/admin/logins")
-	public ResponseEntity<List<LoginHistoryVO>> getAllLoginHistory(@RequestBody LoginHistoryReqeustVO vo) {
-		/*
-		 * {
-		 *  "page" : int		프론트에서 볼 페이지 번호
-		 *  "pageSize" : int	프론트에서 한번에 볼 건수
-		 *  "sort" : "",		정렬기준 "asc" or "desc"
-		 * }
-		 */
-		vo.setStart((vo.getPage()-1) * vo.getPageSize());
-		return new ResponseEntity<>(adminService.findAllLoginHistory(vo), HttpStatus.OK);
+	public ResponseEntity<List<LoginHistoryVO>> getAllLoginHistory() {
+
+		return new ResponseEntity<>(adminService.findAllLoginHistory(), HttpStatus.OK);
 	}
 	
-	// 특정 유저의 로그인 기록 조회
-	@GetMapping("/api/admin/login")
-	public ResponseEntity<List<LoginHistoryVO>> getLoginHistory(@RequestBody LoginHistoryReqeustVO vo) {
-		vo.setStart((vo.getPage()-1) * vo.getPageSize());
-		return new ResponseEntity<>(adminService.findLoginHistory(vo), HttpStatus.OK);
+	// 메뉴 클릭 기록 조회
+	@GetMapping("/api/admin/menu")
+	public ResponseEntity<List<MenuClickVO>> getMenuClick(@RequestBody Map<String, String> data) {
+		/*
+		 * {
+		 *  "period" : "day"
+		 * }
+		 */
+		// default로 7일치 기록을 보여줌
+		if(data.get("period").equals("day")) {
+			System.out.println("day로 진입");
+			return new ResponseEntity<>(adminService.findMenuClickDay(), HttpStatus.OK);
+		} else if(data.get("period").equals("week")) {
+			return new ResponseEntity<>(adminService.findMenuClickWeek(), HttpStatus.OK);			
+		} else if(data.get("period").equals("month")) {
+			return new ResponseEntity<>(adminService.findMenuClickMonth(), HttpStatus.OK);			
+		} else {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
 	}
-
 }
 
 
---
+++ server/iam/src/main/java/com/lhsk/iam/domain/admin/model/vo/MenuClickVO.java
@@ -0,0 +1,20 @@
+package com.lhsk.iam.domain.admin.model.vo;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MenuClickVO {
+	private String menuNm;			// 메뉴이름
+	private long clickCnt;			// 클릭횟수
+	private LocalDate date;			// 집계날짜
+}
