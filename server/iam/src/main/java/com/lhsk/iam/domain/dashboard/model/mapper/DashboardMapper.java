55bfed1c100c32aca2e1c4414389f274923c06a3
dajung8210@gmail.com <dajung8210@gmail.com>
2023-05-11 21:36:03 +0900

    [UPDATE] Implement balance sum by acctDv APIs

--- server/iam/src/main/java/com/lhsk/iam/domain/dashboard/model/mapper/DashboardMapper.java
+++ server/iam/src/main/java/com/lhsk/iam/domain/dashboard/model/mapper/DashboardMapper.java
@@ -1,8 +1,20 @@
 package com.lhsk.iam.domain.dashboard.model.mapper;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
+import com.lhsk.iam.domain.dashboard.model.vo.AcctDvByBalSumVO;
+
 @Mapper
-public class DashboardMapper {
-	
+public interface DashboardMapper {
+	// 관리자의 전체 계좌 구분별 합계 계산
+	List<AcctDvByBalSumVO> findByAdminsAcctDvToBalSum();
+	// 관리자의 총 자산 합계 계산
+	BigDecimal findByAdminsAllAccountToBalSum();
+	// 회원의 보유 계좌 구분별 합계 계산
+	List<AcctDvByBalSumVO> findByUsersAcctDvToBalSum(int userNo);
+	// 회원의 보유 총 자산 합계 계산
+	BigDecimal findByUsersAllAccountToBalSum(int userNo);
 }
--- server/iam/src/main/java/com/lhsk/iam/domain/dashboard/model/vo/AcctDvByBalSumVO.java
+++ server/iam/src/main/java/com/lhsk/iam/domain/dashboard/model/vo/AcctDvByBalSumVO.java
@@ -2,6 +2,11 @@ package com.lhsk.iam.domain.dashboard.model.vo;
 
 import java.math.BigDecimal;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AcctDvByBalSumVO {
 	private String acctDv;		// 계좌 구분(01: 수시 입출금, 02: 예적금, 03: 대출)
 	private BigDecimal balSum;	// 잔액 합계
