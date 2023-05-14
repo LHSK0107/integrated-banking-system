eeb9ea4b7ae78faab9409761d58e95c6a3ad3542
dajung8210@gmail.com <dajung8210@gmail.com>
2023-05-15 04:24:25 +0900

    [UPDATE] implement acctDv01InoutSum APIs in dashboard

--- server/iam/src/main/java/com/lhsk/iam/domain/dashboard/model/vo/AcctDv01InoutSumVo.java
+++ server/iam/src/main/java/com/lhsk/iam/domain/dashboard/model/vo/AcctDv01InoutSumVo.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @Setter
 public class AcctDv01InoutSumVo {
 
+	private String trscDt;		// 거래 일자
 	private String inoutDv;		// 입출금 구분(1: 입금, 2: 출금)
-	private LocalDate trscDt;	// 거래 일자
 	private BigDecimal amtSum;	// 거래금액 합계
 }
