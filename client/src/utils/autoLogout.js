e794baa76bc0e6e7f0acd1b93dbb19ecb5b489e6
hg14699@naver.com <hangyeoldora>
2023-05-20 12:08:50 +0900

    [UPDATE] delete localstorage syntax

--- client/src/utils/autoLogout.js
+++ client/src/utils/autoLogout.js
@@ -1,20 +1,17 @@
 import useAuth from "../hooks/useAuth";
 import axios from "../api/useCommonAxios";
-import {useNavigate} from "react-router-dom";
-const autoLogout = () =>{
+import { useNavigate } from "react-router-dom";
+const autoLogout = () => {
   console.log(`로그아웃`);
-    localStorage.removeItem("jwt")
-    const logout = async () =>{
-        return await axios.post("https://iam-api.site/api/logout",
-          {
-            allAccount: 1,
-            inout: 2,
-            inoutReport: 3,
-            dailyReport: 4,
-            dashboard: 5,
-          }
-        )
-    }
-    return logout;
-}
+  const logout = async () => {
+    return await axios.post("https://iam-api.site/api/logout", {
+      allAccount: 1,
+      inout: 2,
+      inoutReport: 3,
+      dailyReport: 4,
+      dashboard: 5,
+    });
+  };
+  return logout;
+};
 export default autoLogout;
