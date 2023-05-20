beca9e270e556959aea37acf4e0d922130cbe848
poohharry <81311834+poohharry@users.noreply.github.com>
2023-05-21 05:55:00 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 echo "ApplicationStart stage!!!!!!!"
-nohup java -jar /home/ubuntu/github_action/iam-0.0.1-SNAPSHOT.jar &
+nohup java -jar /home/ubuntu/github_action/iam-0.0.1-SNAPSHOT.jar > /home/ubuntu/logfile.out 2>&1 &
