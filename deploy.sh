5456bb29a34bb65a9eab3f1e0585b60c648a1127
poohharry <81311834+poohharry@users.noreply.github.com>
2023-05-21 05:44:47 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
 echo "ApplicationStart stage!!!!!!!"
-cd /home/ubuntu/github_action
-nohup java -jar ~/aws_deploy/iam-0.0.1-SNAPSHOT.jar &
+nohup java -jar /home/ubuntu/github_action/iam-0.0.1-SNAPSHOT.jar &
