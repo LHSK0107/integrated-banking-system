e7d5f93e7dc64dc7c3b05ce1c18aa6574aefa750
poohharry <81311834+poohharry@users.noreply.github.com>
2023-05-21 14:39:46 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,16 @@
 echo "ApplicationStart stage!!!!!!!"
+
+jarname="iam-0.0.1-SNAPSHOT"
+
+# 프로세스 id 얻기
+pid=$(ps -ef | grep $jarname | grep -v grep | awk '{ print $2 }')
+
+# 프로세스 id가 존재하면 종료
+if [ -n "$pid" ]; then
+    echo "Killing process with id: $pid"
+    kill $pid
+else
+    echo "No running process found for $jarname"
+fi
+
 nohup java -jar /home/ubuntu/github_action/iam-0.0.1-SNAPSHOT.jar > /home/ubuntu/logfile.out 2>&1 &
